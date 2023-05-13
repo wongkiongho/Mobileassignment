918efda731d05f82d3eb1a07267e3c97a1d34b42
DuBronC <cheongty-wp20@student.tarc.edu.my>
2023-05-14 04:12:03 +0800

    Firebase

--- app/src/main/java/my/edu/tarc/mobileapplicationassignment/HomeFragment.kt
+++ app/src/main/java/my/edu/tarc/mobileapplicationassignment/HomeFragment.kt
@@ -1,59 +1,47 @@
 package my.edu.tarc.mobileapplicationassignment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var mDatabase: DatabaseReference
+    private lateinit var mEditName: EditText
+    private lateinit var mEditPhone: EditText
+    private lateinit var mEditEmail: EditText
+    private lateinit var mButton: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("User")
+        mEditName = view.findViewById(R.id.editTextTextPersonName2)
+        mEditPhone = view.findViewById(R.id.editTextPhone)
+        mEditEmail = view.findViewById(R.id.editTextTextEmailAddress)
+        mButton = view.findViewById(R.id.button3)
+
+        mButton.setOnClickListener {
+            val name = mEditName.text.toString()
+            val phone = mEditPhone.text.toString()
+            val email = mEditEmail.text.toString()
+            val user = User(name, phone, email)
+            mDatabase.child(name).setValue(user)
+        }
     }
 }
+
+data class User(
+    val name: String? = "",
+    val phone: String? = "",
+    val email: String? = ""
+)
