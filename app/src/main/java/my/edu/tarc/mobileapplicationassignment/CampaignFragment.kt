1a554c16f3e72d53e34468dc22ef08eea8ae7d8a
ZiKian002120 <chiazk-wm21@student.tarc.edu.my>
2023-05-13 23:22:12 +0800

    Campaign

--- app/src/main/java/my/edu/tarc/mobileapplicationassignment/CampaignFragment.kt
+++ app/src/main/java/my/edu/tarc/mobileapplicationassignment/CampaignFragment.kt
@@ -1,10 +1,16 @@
 package my.edu.tarc.mobileapplicationassignment
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import java.util.Calendar
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.net.Uri
+import android.widget.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,7 +26,7 @@ class CampaignFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    private val PICK_IMAGE_REQUEST = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,7 +40,90 @@ class CampaignFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_campaign, container, false)
+        val view = inflater.inflate(R.layout.fragment_campaign, container, false)
+
+        // Get the "Select Date" button and "textView7"
+        val selectDateButton = view.findViewById<Button>(R.id.datebtn)
+        val textView7 = view.findViewById<TextView>(R.id.textView7)
+
+
+        // Get the "Select Image" button
+        val selectImageButton = view.findViewById<Button>(R.id.upload_image)
+
+        selectImageButton.setOnClickListener {
+            // Create an Intent to pick an image from the gallery
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+
+            // Start the Intent and wait for a result
+            startActivityForResult(intent, PICK_IMAGE_REQUEST)
+        }
+
+
+        // Set an onClickListener for the button
+        selectDateButton.setOnClickListener {
+            // Create a new DatePickerDialog
+            val calendar = Calendar.getInstance()
+            calendar.add(Calendar.DAY_OF_MONTH, 1) // set minimum date to tomorrow
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+            val datePickerDialog = DatePickerDialog(
+                requireContext(),
+                { view, year, month, dayOfMonth ->
+                    // Do something with the selected date
+                    val selectedDate = "${dayOfMonth}/${month + 1}/${year}"
+                    textView7.text = selectedDate // set the selected date to the textView7
+                },
+                year,
+                month,
+                day
+            )
+
+            // Set the minimum date
+            datePickerDialog.datePicker.minDate = calendar.timeInMillis
+
+            // Show the dialog
+            datePickerDialog.show()
+        }
+
+        val categories = listOf("Category 1", "Category 2", "Category 3", "Category 4")
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, categories)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val categorySpinner = view.findViewById<Spinner>(R.id.category_spinner)
+        categorySpinner.adapter = adapter
+
+        categorySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                // Handle the user selection
+                val selectedCategory = categories[position]
+                Toast.makeText(requireContext(), "You selected $selectedCategory", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do nothing
+            }
+        }
+
+
+
+        return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
+            // Get the selected image
+            val imageUri = data.data
+
+            uploadImage(imageUri)
+        }
+
+    }
+
+    private fun uploadImage(imageUri: Uri?) {
+
     }
 
     companion object {
@@ -57,3 +146,13 @@ class CampaignFragment : Fragment() {
             }
     }
 }
+
+
+
+
+
+
+
+
+
+
